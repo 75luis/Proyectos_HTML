a445797ea5d0e3ec55ce7caf757823b8ef7f4530
Luis <luis.chaparin@gmail.com>
2023-02-28 16:06:00 -0500

    Proyectos del curso

---
+++ prueba.js
@@ -0,0 +1,16 @@
+
+function sumar() {
+    a = document.getElementById('txta').value;
+    b = document.getElementById('txtb').value;
+    c = parseInt(a) + parseInt(b);
+    document.getElementById("txtresultado").innerHTML = c;
+    console.log.a;
+    console.log.b;
+    console.log("desde la consola: ");
+    alert("Archivo externo")
+
+}
+function saludo(nombre) {
+    alert(nombre);
+}
+
